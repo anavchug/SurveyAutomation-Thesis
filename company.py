7611b3e5d57e193e38d817b737918f5522310bf8
Anav Chug <ac32809@georgiasouthern.edu>
2023-05-16 22:57:32 -0400

    Routing Works and Data inserted correctly in sql

--- company.py
+++ company.py
@@ -35,11 +35,14 @@ def index():
         phone = request.form["phone"]
         prompt = request.form["prompt"]
 
+        # print(name, branch, email, phone, prompt)
         # Insert company data into the database
         sql = (
             "INSERT INTO companies (name, branch, email, phone) VALUES (%s, %s, %s, %s)"
         )
+        print(cursor.rowcount)
         values = (name, branch, email, phone)
+        print(values)
         cursor.execute(sql, values)
         mydb.commit()
 
@@ -48,59 +51,83 @@ def index():
 
         # Insert the prompt into the company_prompts table with the corresponding companyId value
         sql = "INSERT INTO company_prompts (companyId, prompt) VALUES (%s, %s)"
+        print(cursor.rowcount)
         values = (companyId, prompt)
+        print(values)
         cursor.execute(sql, values)
         mydb.commit()
 
-        # Generate questions based on the prompt and display it on the page
         generated_questions = generate_question(prompt)
         print("Generated questions", generated_questions)
 
         for question in generated_questions:
-            # Extract the question text
+            # Extract the questions and format them to remove extra spaces
             question_text = question.split("?")[0].strip() + "?"
             formatted_questions.append(question_text)
             print("Formatted questions", formatted_questions)
 
-            # Extract the options and format them
-            options = question.split("?")[1].strip().split("\n\n")
-            formatted_options = []
-            for option in options:
-                formatted_option = option.split(")")[1].strip()
-                formatted_option = re.sub(r"\..*", "", formatted_option)
-                formatted_options.append(formatted_option)
-                formatted_answers.append(formatted_options)
+            # Extract the options and format them to remove extra spaces
+            options = re.split(r"[\n\r]+", question.split("?")[1].strip())
+            if options:
+                formatted_answers.append(
+                    [option.split(")")[1].strip() for option in options if option]
+                )
             print("Formatted answers", formatted_answers)
 
-        # Show these questions on a new webpage in the browser
+        # return "Form submitted"
+        return redirect(url_for("survey_questions"))
+    else:
+        return render_template("Company Interface.html")
+
+
+@app.route("/survey_questions", methods=["GET", "POST"])
+def survey_questions():
+    # Generate the survey questions based on the prompt and display it on the page
+    if request.method == "POST":
+        # Extract the questions to be deleted
+        delete_list = []
+        for key, value in request.form.items():
+            if key.startswith("delete_"):
+                delete_list.append(int(key.split("_")[1]))
+
+        # Remove the deleted questions from the formatted lists
+        formatted_questions_final = []
+        formatted_answers_final = []
+        for i, question in enumerate(formatted_questions):
+            if i not in delete_list:
+                formatted_questions_final.append(question)
+                formatted_answers_final.append(formatted_answers[i])
+                print(formatted_questions_final)
+                print(formatted_answers_final)
+
+        # Update the global formatted_questions and formatted_answers lists
+        # global formatted_questions
+        # global formatted_answers
+        # formatted_questions = formatted_questions_final
+        # formatted_answers = formatted_answers_final
+
+        # Insert the remaining questions into the database
+        question1 = formatted_questions_final[0]
+        question2 = formatted_questions_final[1]
+        companyId = cursor.lastrowid
+        sql = "INSERT INTO Questions (companyId, question1, question2) VALUES (%s, %s, %s)"
+        values = (companyId, question1, question2)
+        cursor.execute(sql, values)
+        mydb.commit()
+
+        return redirect(url_for("finalize_questions"))
+    else:
         return render_template(
             "SurveyQuestions.html",
-            formatted_questions=formatted_questions,
-            formatted_answers=formatted_answers,
+            question_data=zip(formatted_questions, formatted_answers),
         )
 
-        # Redirect to the generated_questions function to display the survey
-        # return redirect(url_for('generatedQuestions', generated_questions=generated_questions))
-    return render_template("Company Interface.html")
 
-
-# Handle form submissions
-@app.route("/submit", methods=["POST"])
-def submit():
-    answers = {}
-    for question_id in request.form:
-        answers[int(question_id)] = request.form[question_id]
-    print(answers)
-    return "Thanks for submitting the survey!"
-
-
-# Render the generated questions form
-@app.route("/generated_questions", methods=["GET", "POST"])
-def generatedQuestions():
+@app.route("/finalize_questions", methods=["GET", "POST"])
+def finalize_questions():
     if request.method == "POST":
-        # Process the user survey form data on form submission
-        # try:
-        # Get form data
+        # Redirect the user to generated Questions
+        # return redirect(url_for("generatedQuestions"))
         firstname = request.form["firstname"]
         lastname = request.form["lastname"]
         email = request.form["email"]
@@ -116,21 +143,39 @@ def generatedQuestions():
         mydb.commit()
 
         # Return success message
-        return "Survey submitted successfully"
+        return "Your responses have been recorded"
 
-    # except:
-    # Return error message if survey data could not be submitted
-    # return 'Error submitting survey data'
+    else:
+        # Render the generated questions page
+        return render_template(
+            "GeneratedQuestions.html",
+            question_data=zip(formatted_questions, formatted_answers),
+        )
+
+
+# Render the generated questions form
+
+"""
+@app.route("/generated_questions", methods=["GET", "POST"])
+def generatedQuestions():
+    if request.method == "POST":
+        # Process the user survey form data on form submission
+        # Get form data
+        return "Survey Submitted"
 
     else:
         # Get the generated questions from the query string parameters
-        generated_questions = request.args.getlist("generated_questions")
+        formatted_questions = request.args.getlist("formatted_questions")
+        formatted_answers = request.args.getlist("formatted_answers")
         print(generated_questions)
 
         # Render the generated questions template
         return render_template(
-            "GeneratedQuestions.html", generated_questions=generated_questions
+            "GeneratedQuestions.html",
+            formatted_questions=formatted_questions,
+            formatted_answers=formatted_answers,
         )
+"""
 
 
 # Define a function to generate questions based on the prompt and return them in a list
